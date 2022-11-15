83a6419620481222436fbeea320ba34c3bf2a24c
Islam <islam99kz@gmail.com>
2022-11-16 02:57:29 +0600

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -8,24 +8,26 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-      .setTitle("BookStore app by Islam, Danyal & Nazym")
-      .setDescription("Documentation for REST API v1")
-      .setVersion("1.0.0")
-      .addTag("BookStore")
-      .build();
-  app.use(bodyParser.json({limit: '50mb'}));
-  app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-  const documentation = SwaggerModule.createDocument(app,config)
-  SwaggerModule.setup("/docs",app,documentation)
+    .setTitle("BookStore app by Islam, Danyal & Nazym")
+    .setDescription("Documentation for REST API v1")
+    .setVersion("1.0.0")
+    .addTag("BookStore")
+    .build();
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+  const documentation = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup("/docs", app, documentation)
 
   await app.listen(8080);
-  app.enableCors({
-    origin: 'localhost:5555',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  })
+  app.enableCors(
+    // {
+    //   origin: '*',
+    //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    //   // credentials: true,
+    // }
+  )
   app.useGlobalPipes(new MyValidationPipe());
   app.setGlobalPrefix("api/v1");
-  
+
 }
 bootstrap();
