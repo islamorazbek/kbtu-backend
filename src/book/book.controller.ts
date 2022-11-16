9aaeecf28dc33c20978a872afabb2a8306c34e85
Islam <islam99kz@gmail.com>
2022-11-16 19:31:16 +0600

    fix get book

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Post, Put } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { Book } from './book.entity';
 import { BookService } from './book.service';
 import { CreateBookDto } from './dto/create-book.dto';
@@ -9,18 +10,24 @@ export class BookController {
 
   constructor(private bookService: BookService) { }
 
+  @ApiOperation({ summary: "creata a book" })
+  @ApiResponse({ status: 201, type: Book, description: "Create book response" })
   @Post()
   createBook(@Body() dto: CreateBookDto): Promise<Book> {
     return this.bookService.createBook(dto)
   }
 
+  @ApiOperation({ summary: "update a book" })
+  @ApiResponse({ status: 201, type: Book, description: "Update book response" })
   @Put()
   updateBook(@Body() dto: EditBookDto): Promise<Book> {
     return this.bookService.editBook(dto)
   }
 
+  @ApiOperation({ summary: "update a book" })
+  // @ApiResponse({ status: 200, type: {Book[]}, description: "Update book response" })
   @Get()
-  getBooks(): Promise<Book[]> {
+  getBooks(): Promise<{ books: Book[] }> {
     return this.bookService.getBooks()
   }
 
